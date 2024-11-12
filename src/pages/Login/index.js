2773c97b06f2b19d18e8d07044ce485dbc131e4a
Rafaelm <isaizanami@gmail.com>
2024-11-11 21:50:28 -0300

    criado a page de Login e estilizada, configurada para ser funcional, so fazer Login se tiver criado Conta.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,27 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import { isEmail } from "validator";
 import { useDispatch } from "react-redux";
+import { useNavigate, useLocation } from "react-router-dom";
 
-import { Title } from "./styled";
 import { Container } from "../../styles/GlobalStyles";
-import * as exampleActions from "../../store/modules/example/action";
+import { Form } from "./styled";
+import * as actions from "../../store/modules/auth/action";
 
 export default function Login() {
   const dispatch = useDispatch();
-  function handleClick(e) {
-    e.preventDefault();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    dispatch(exampleActions.clicaBotaoRequest());
-  }
+  // Obtém o `prevPath` da `location.state`, ou define "/" como padrão
+  const prevPath = location.state?.prevPath || "/";
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      formErrors = true;
+      toast.error("E-mail inválido");
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      formErrors = true;
+      toast.error("Senha inválida");
+    }
+
+    if (formErrors) return;
+
+    // Envia o `navigate` e `prevPath` para a saga
+    dispatch(actions.loginRequest({ email, password, prevPath, navigate }));
+  };
+
   return (
     <Container>
-      <Title>
-        Login
-        <small>Oie</small>
-      </Title>
-      <p>Lorem ipsum dolor sit</p>
-      <button type="button" onClick={handleClick}>
-        Enviar
-      </button>
+      <h1>Login</h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Digite seu email"
+        />
+
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Digite sua senha"
+        />
+
+        <button type="submit">Acessar</button>
+      </Form>
     </Container>
   );
 }
--- src/pages/Login/styled.js
+++ src/pages/Login/styled.js
@@ -1,3 +1,19 @@
 import styled from "styled-components";
+import * as color from "../../config/color";
 
-export const Title = styled.h1``;
+export const Form = styled.form`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+
+  input {
+    margin-bottom: 20px;
+    height: 40px;
+    padding: 0 10px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    &:focus {
+      border: 1px solid ${color.primaryColor};
+    }
+  }
+`;
